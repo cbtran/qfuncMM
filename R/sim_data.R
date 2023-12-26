a4073c20be889ce8210998ba879e8fdedecc09ab
roobnloo <71776622+roobnloo@users.noreply.github.com>
2023-12-26 19:09:09 +0000

    add simulated example data

---
+++ R/sim_data.R
@@ -0,0 +1,14 @@
+#' Simulated voxel-level BOLD signals
+#'
+#' Simulated data from three regions using real voxel coordinates from rats.
+#' The regions have 41, 25, and 77 voxels with 60 time points.
+#' Data are generated from the mixed model strong inter-regional signal
+#' and weak intra-regional spatial correlation.
+#'
+#' @format ## `sim_data`
+#' A list with two elements:
+#' \describe{
+#'   \item{data}{List of data matrices for each region}
+#'   \item{coords}{Matrix giving voxel coordinates for each region}
+#' }
+"sim_data"
